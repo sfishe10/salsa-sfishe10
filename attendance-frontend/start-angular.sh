f3846e4b25fca51cd64f22460d239c3d9f384071
Ubuntu <ubuntu@ip-172-31-32-165.us-west-2.compute.internal>
2026-07-08 17:25:15 +0000

    updated port in start-angular.sh

--- attendance-frontend/start-angular.sh
+++ attendance-frontend/start-angular.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd ~/salsa/attendance-frontend/dist/client/browser
-npx serve -s . -l 444 \
+npx serve -s . -l 443 \
   --ssl-cert "/etc/letsencrypt/live/807.band/fullchain.pem" \
   --ssl-key "/etc/letsencrypt/live/807.band/privkey.pem"
