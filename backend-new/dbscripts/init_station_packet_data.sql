0004d876a5eead71af63d395b410d91286623757
sfishe10 <sfishe10@calpoly.edu>
2026-07-20 10:02:49 -0700

    init station packet data script

---
+++ backend-new/dbscripts/init_station_packet_data.sql
@@ -0,0 +1,374 @@
+INSERT INTO StationPacket (packetId, stationId, title, role, info, content, level)
+VALUES
+    (NULL, 1, 'Instructor Setup', 'instructor', 'setup', 'Station 1: Stationary Basics
+
+1.	Standby
+a.	Right arch remains on dot
+b.	Attention remains on director/instructor
+2.	Attention
+a.	1 count move
+b.	Heels together
+c.	Toes apart at 45-degree angle (fist in between arches should be good angle)
+d.	Knees straight and unlocked
+e.	Hips centered above heels, hips rotated forward
+f.	Core engaged
+g.	Elbows relaxed and bent slightly
+h.	Relaxed fists at sides with thumbs pointing down
+i.	Shoulders back and relaxed
+j.	Chin up, eyes above the horizon (L check teaching technique)
+k.	Proud demeanor
+l.	Not talking
+m.	Vocal in time “one”
+n.	Attention with instrument should leave plenty of space between the instrument and player’s chest (except for certain sections) like hugging a yoga ball to maintain a proud, not collapsed, look
+3.	Band-Set
+a.	1 count move
+b.	Come to the attention position on count 1
+c.	No Cal Poly High Step
+d.	“Band-Set” “1”
+4.	Parade Rest
+a.	1 count move
+b.	Hands clasped together at belt level (Left over right) (differs per instrument)
+c.	Left foot moves out to shoulder width (without picking up or dragging), right stays planted
+d.	Feet stay at 45 degrees
+e.	Upper body remains in attention position
+f.	Attention remains on director/instructor
+g.	Verbal
+i.	“1”
+5.	Called Horns Up/Down
+a.	1 count move
+b.	Without instrument:
+i.	Hands snapped from attention position
+ii.	Left hand falls over right fist, left knuckles aligned on top of right knuckles
+iii.	Arms now form “triangle of power”
+1.	Elbows are not collapsed in or flayed out, instead they are slightly outside the body to form a nearly equilateral triangle
+iv.
+c.	With instrument
+i.	Individual for section, jist:
+1.	Instrument comes from attention position into “ready to play” position
+d.	Rest of body remains in attention position
+e.	For Horns down, reverse the Horns up command
+6.	Visual Horns Up/Down
+a.	1 count move
+b.	Move from horns up to horns down given the visual signals:
+i.	“Dut Dut Dut”
+ii.	Cut off “and Down”
+iii.	Verbals
+1.	Up: “Horns Up”
+2.	Down: “And Down”
+7.	Slow Horns Up/Down
+a.	Move from horns up to horns down given the visual signals
+i.	Raise and lower hands at uniform speed
+ii.	Speed of instrument raising/lowering matches the speed of the director’s hands
+8.	Kick-Down-Up
+a.	3 Count move
+b.	Count 1
+i.	The right foot kicks out to approximately a thirty-degree angle, knee is straight, foot remains at the same angle (toes not pointed or up)
+c.	Count 2
+i.	The right foot is returned to the attention position
+ii.	The player’s horn is brought to the “ready” position (Similar to the attention position for most sections)
+d.	Count 3
+i.	The horn is brought to the horns up position
+e.	Verbals
+i.	“Kick-Down-Up”
+9.	Carry Position
+a.	Varies from section to section, consult section leaders for latest information 
+', 0),
+    (NULL, 1, 'Instructor Script', 'instructor', 'script', 'This is your station 1 evaluation. Please relax and follow my commands.
+
+(Give a moderate tempo, 130-150 BPM)
+Band set
+Parade rest
+Band set
+Horns Up
+Horns Down
+Visual Horns Up
+Visual Horns Down
+Slow horns up
+Slow horns down
+Kick Down Up
+', 1),
+    (NULL, 1, 'Evaluator Setup', 'evaluator', 'setup', '', 2),
+    (NULL, 1, 'Evaluator Script', 'evaluator', 'script', '', 3),
+    (NULL, 2, 'Instructor Setup', 'instructor', 'setup', 'Station 2: Stationary Basics Pt. 2
+
+10.	Left Face
+a.	2 Count Move
+b.	Count 1
+i.	The left toe is raised off the ground
+ii.	The right heel is raised off the ground
+iii.	The player is now balanced on their left heel and right toe
+iv.	The player pivots 90 degrees to the left by pushing with their right toe
+1.	The knee may bend during this maneuver
+c.	Count 2
+i.	The player brings the right foot forward to meet the left and is returned to the attention position
+d.	The upper body remains stable and in the attention position throughout this process by engaging the core
+i.	Players may find it difficult to not swing their arms. Discourage this behavior by inviting players to grab their pockets or the hem of their shirts
+e.	Verbal
+i.	“1-2”
+11.	Right Face
+a.	2 Count Move
+b.	Count 1
+i.	The right toe is raised off the ground
+ii.	The left heel is raised off the ground
+iii.	The player is now balanced on their right heel and left toe
+iv.	The player pivots 90 degrees to the right by pushing with their left toe
+1.	The knee may bend during this maneuver
+c.	Count 2
+i.	The player brings the left foot forward to meet the right and is returned to the attention position
+d.	The upper body remains stable and in the attention position throughout this process by engaging the core
+e.	Verbal
+i.	“1-2”
+12.	About Face
+a.	3 Count Move
+b.	Count 1
+i.	The left foot is moved approximately 18 inches (slightly less than 8/5 step in front of the right foot, foot at a parallel angle to the right foot (step size will take some practice)
+ii.	Roll through the step so the right and left heels come off the ground such that the player is balancing on their platforms
+1.	NOTE: Do NOT cross the right foot over the left in this count
+c.	Count 2
+i.	The player pivots over their right shoulder by pushing with their left foot.
+1.	NOTE: Encourage proper upper body form by reminding players to tighten their core and not swing their arms (see left and right face)
+d.	Count 3
+i.	The left foot is brought forward to meet the right in the attention position
+e.	Verbal
+i.	“Len-Len-Len”
+13.	Mark Time
+a.	The left heel is raised on the “and” of the previous beat
+b.	The left toe then leaves the ground and the foot moves to a zero-degree angle, straight ahead
+c.	The left heel then strikes the ground on count 1 and the right heel is raised and the right foot moves to be parallel to the left
+d.	The right heel strikes the ground on count 2 and the left heel is raised, but the platforms and toes do not leave the ground anymore, only heels and arch
+e.	This continues in a fluid motion for the duration of the move
+f.	NOTE: The heel should be raised to approximately ankle bone height. Further, the move should be smooth, and the ankle bones should cross each other on the and of each beat. Be aware of swaying side to side or heavy heel strikes that make the player’s sound bumpy.
+g.	Verbal:
+i.	“And-1”
+14.	Halting
+a.	2 Count move
+b.	Count 1
+i.	The right foot is raised fully off the ground and planted back in the attention position
+c.	Count 2
+i.	The left foot is raised fully off the ground and planted back in the attention position
+d.	Verbal
+i.	“Step and Close”
+15.	Slow Turns
+a.	These moves must also follow the mark time form
+b.	Variable count move
+c.	For the given counts:
+d.	Maintaining mark time form, rotate the body at a constant speed through the angle requested.
+e.	Example: 4 count 90 degrees to the left
+i.	Count 1
+1.	Initiate turning motion by lifting full foot off the ground and replanting
+ii.	Count 2
+1.	Hit 45 degrees between start and end points
+iii.	Count 4
+1.	Returned to attention position with body facing 45 degrees to the left of initial position 
+', 0),
+    (NULL, 2, 'Instructor Script', 'instructor', 'script', 'This is your station 2 evaluation. Please relax and follow my commands.
+
+(Give a moderate tempo, 130-150 BPM)
+Left Face
+Right Face
+About Face
+About Face
+Mark time 16cts
+Halt
+Slow turn right 90Deg 4cts
+Slow turn left 90Deg 4cts
+', 1),
+    (NULL, 2, 'Evaluator Setup', 'evaluator', 'setup', '', 2),
+    (NULL, 2, 'Evaluator Script', 'evaluator', 'script', '', 3),
+    (NULL, 3, 'Instructor Setup', 'instructor', 'setup', 'Station 3: Beginnings of Movement
+16.	Forward March
+a.	We follow the “glide step” marching form
+i.	The feet remain low and close to the ground
+ii.	The knees remain mostly unbent, however some bending is allowed for comfort and maintaining the form
+iii.	The heel strikes on each downbeat
+iv.	After the heel strikes, the player roles through the foot slowly transitioning the weight from heel to toe such as that on the next downbeat, the player’s entire weight is resting on the toe
+v.	The size of each standard 8-5 step is 22.5” from arch to arch
+1.	This step remains consistent across the entire five-yard interval
+b.	The upper body remains in the attention position throughout the entire move (the goal is to keep the upper body free from sway and bounce that affects the player’s sound)
+c.	NOTE: Encourage players to practice balancing on the toes/ball of one foot and the heel of another. This will encourage proper form.
+d.	Verbal
+i.	“And one”
+17.	Left Flank
+a.	Equate the left flank to a left hace, but while in motion
+b.	2 Count Move
+i.	Count 1
+1.	The right foot executes a full regular glide step  and the player’s weight is shifted to their platforms. The left foot begins to leave the ground as if for another normal forward step. No cheat steps!
+2.
+ii.	Count And of 1
+1.	On the and of one, the ankle bones come together with the left foot on the inside of the turn (the leg shouldn’t swing out)
+2.	The player pivots on their right platform ninety degrees to the left.
+iii.	Count 2
+1.	On the downbeat of count 2, the left heel strikes the ground in the first step of a forward march in the new direction.
+18.	Right Flank
+a.	Equate the right flank to a right hace, but while in motion
+b.	2 Count Move
+i.	Count 1
+1.	The right foot executes a full regular glide step  and the player’s weight is shifted to their platforms. The left foot begins to leave the ground as if for another normal forward step.
+2.
+ii.	Count And of 1
+1.	On the and of one, the ankle bones come together with the left foot on the outside of the turn (the leg shouldn’t swing out).
+2.	The player pivots on their right platform ninety degrees to the right.
+iii.	Count 2
+1.	On the downbeat of count 2, the left heel strikes the ground in the first step of a forward march in the new direction.
+c.	NOTE: It is common for players to “swing their left foot wide.” Discourage this behavior by having players pause on the and of count 1 with their ankles together.
+19.	To the Rear
+a.	Equate to the rear to an about face, but in motion
+b.	2 Count Move
+i.	Count 1
+1.	The right foot executes a full regular glide step  and the player’s weight is shifted to their platforms. The left heel begins to leave the ground as if for another normal forward step.
+ii.	Count And of 1
+1.	The player pivots on their platforms 180 degrees over their left shoulder.
+iii.	Count 2
+1.	The left foot is replanted adjusting to a full-size step beginning with the heel.
+2.	This initiates the next “forward march” maneuver
+c.	NOTE: The ankle bones do NOT meet on the and of 1 of this move!
+20.	Halting
+a.	2 Count Move
+i.	Count 1
+1.	The right foot executes an adjusted roll step . This roll step follows normal form, but places the right foot in the attention position
+ii.	Count 2
+1.	The left foot executes a second adjusted roll step to meet the right foot and return to the attention position.
+b.	NOTE: Players may have a tendency to sway forwards and backwards as their forward momentum is halted. Combat this by instructing players to engage their cores.
+c.	Verbal
+i.	“Step and Close”
+', 0),
+    (NULL, 3, 'Instructor Script', 'instructor', 'script', 'This is your station 3 evaluation. Please relax and listen to my instructions.
+
+(Give a moderate tempo, 120-130 BPM)
+PART 1
+Mark time 8
+Forward march 8
+(Repeat those instructions until I tell you to Halt)
+Halt
+
+PART 2
+Forward March 8
+Left flank
+Forward March 8
+To the rear
+Forward March 8
+Right Flank
+Forward March 8
+Halt
+
+', 1),
+    (NULL, 3, 'Evaluator Setup', 'evaluator', 'setup', '', 2),
+    (NULL, 3, 'Evaluator Script', 'evaluator', 'script', '', 3),
+    (NULL, 4, 'Instructor Setup', 'instructor', 'setup', 'Station 4: Directions of Movement
+21.	Adjusted Step Sizes
+a.	6-5
+i.	This step size is larger than 8-5
+ii.	The full form from “Forward Marching” should be maintained despite the change in step size
+1.	Pay particular attention to players who may overstep early and correct by collapsing their step size in the end. Combat this by having players complete the exercise with their eyes closed.
+b.	12-5
+i.	This step size is smaller than 8-5
+ii.	The full form from “Forward Marching” should be maintained despite the change in step size
+1.	Pay particular attention to players who may fail to lift their foot off the ground and instead “shuffle” combat this by gradually shrinking the step size down to 12-5
+22.	Backwards March
+a.	We follow the “backwards glide step” form
+i.	Players are always up on their platforms. This means the heels are off the ground slightly, with the full weight on the balls of the feet.
+1.	The heels should not be far off the ground, hovering just enough for clearance.
+ii.	For each step, the player lifts the corresponding foot and pushes backwards. The heel does 	NOT touch the ground when the foot is replanted.
+iii.	Step size is specified. However, for exercises use 8-5.
+b.	The upper body remains in the attention position. Encourage proper form by reminding players to engage their core.
+c.	NOTE: Most players will under-step the 8-5. Discourage this by having players push their feet back farther especially on count 1.
+23.	Transitioning Between Forwards and Backwards
+a.	Forwards to Backwards
+i.	1 count move
+ii.	On the final step of the forward march, players execute a normal roll step
+iii.	On the and in-between the last forward step and first backwards step, players come up on the platforms
+iv.	The left foot lifts slightly and is replanted on the first count of the backwards march
+v.	NOTE: Balance is often an issue in this maneuver, remind players to engage their core.
+b.	Backwards to Forward
+i.	1 count move
+ii.	On the final step of the backwards march, players execute a normal backwards step on their platforms
+iii.	On the and in-between the last backward step and the first forward step, players come back down to their right heel.
+iv.	The left foot lifts slightly and the heel is replanted on the first count of the backwards march.
+v.	NOTE: Balance is often an issue in this maneuver, remind players to engage their core. 
+', 0),
+    (NULL, 4, 'Instructor Script', 'instructor', 'script', 'This is your station 4 evaluation. Please relax and listen to my instructions.
+
+(Give a moderate tempo, 120-130 BPM)
+PART 1
+Backwards march 8 cts at 8 to 5
+Forward march 8 cts at 8 to 5
+Backwards march 8 cts at 8 to 5
+Forward march 8 cts at 8 to 5
+Halt
+
+PART 2
+Forward march 8 cts at 8 to 5
+Forward march 12cts at 12 to 5
+Forward march 6cts at 6 to 5
+Halt
+', 1),
+    (NULL, 4, 'Evaluator Setup', 'evaluator', 'setup', '', 2),
+    (NULL, 4, 'Evaluator Script', 'evaluator', 'script', '', 3),
+    (NULL, 5, 'Instructor Setup', 'instructor', 'setup', 'Station 5: Intermediate Marching Technique
+24.	Forward Left Slide
+a.	The Forward Left Slide follows all the lower body form of the forward march command
+b.	In the Forward Left Slide, the feet move to the players left while the upper body remains facing forward.
+c.	This is accomplished through a rotation of the hips and shoulders. The shoulders remain perpendicular to front while the hips are at an approximate 45 degree angle to the front. This is essential to achieving full rotation! Work with the group on turning their hips AND shoulders to 45 degrees, then isolate the shoulder rotation continuing to 90 degrees as the hips stay at 45.
+d.	On the first step of a Forward Left Slide, the left foot kicks out to the players left.
+25.	Forward Right Slide
+a.	The Forward Right Slide follows all the lower body form of the forward march command
+b.	In the Forward Right Slide, the feet move to the players right while the upper body remains facing forward.
+c.	On the first step of a Forward Left Slide, the left foot comes in front of the right and is planted to the right of the right foot.
+26.	Backward Left Slide
+a.	The Backward Left Slide follows all the lower body form of the backward march command
+b.	In the Backward Left Slide, the feet move to the players left while the upper body remains facing forward.
+c.	On the first step of a Backward Left Slide, the left foot kicks out to the players left and the platform is planted.
+27.	Backward Right Slide
+a.	The Backward Right Slide follows all the lower body form of the forward march command
+b.	In the Backward Right Slide, the feet move to the players right while the upper body remains facing forward.
+28.	On the first step of a Backward Right Slide, the left foot comes behind the right foot and the platform is planted to the right of the player’s right foot.
+a.	Slide Transitions For EVERY transition between forwards/backwards/slide marching, the right foot pivots on the and count of beat 8 in preparation for the next direction of travel, just like for flank maneuvers. Really emphasize the vocal “and” in time with the movement of the right heel
+b.	The left leg should never swing, it should meet the right heel on the “and” beat just like in a flank maneuver.
+
+ 
+', 0),
+    (NULL, 5, 'Instructor Script', 'instructor', 'script', 'This is your station 5 evaluation. Please relax and listen to my instructions. All movements are at an 8 to 5 step size, horns go up the step-off (not mark time) and down on the halt.
+
+(Give a moderate tempo, 120-130 BPM)
+Forwards 8, forward right slide 8, forward 8, forward left slide 8, halt. Repeat instructions again by default, repeat a second or third time if needed.
+
+Backward right slide 8, backward 8, backward left slide 8, backward 8, halt. Repeat instructions again by default, repeat a second or third time if needed.
+
+Forward 8, backward right slide 8, backward 8, forward right slide 8, halt. Repeat instructions again by default, repeat a second or third time if needed.
+', 1),
+    (NULL, 5, 'Evaluator Setup', 'evaluator', 'setup', '', 2),
+    (NULL, 5, 'Evaluator Script', 'evaluator', 'script', '', 3),
+    (NULL, 6, 'Instructor Setup', 'instructor', 'setup', 'Station 6: Advanced Marching Technique
+
+29.	High Step form
+a.	The left heel, arch, and platform should come off the ground so that the toe is pointed to the ground, with the two halves of the leg forming a 135 degree angle
+b.	This angle between the thigh and calf is maintained as the toe is lifted, and the leg is raised so that the thigh is perpendicular to the player, flat and parallel to the ground
+c.	The toes should remain pointed throughout the entire movement, creating a straight line down from the knee
+30.	Cal Poly Attention
+a.	Four Count Move
+i.	“Ten”
+1.	The left leg is raised in a Cal Poly High Step Form on the prep beat
+ii.	“Hut-1-2-3”
+1.	Three Cal Poly High Step steps are taken in place with the last step replanting the feet in the attention position
+31.	Cal Poly Mark Time
+a.	Ha. Good Luck.
+32.	Cal Poly High Step
+a.	Just Say goodbye to your legs.
+33.	Run On
+a.	AKA Death.
+
+', 0),
+    (NULL, 6, 'Instructor Script', 'instructor', 'script', 'This is your station 6 evaluation. Please relax and listen to my instructions.
+
+(Give a moderate tempo, 120-130 BPM)
+Cal Poly Attention
+Cal Poly High High Step Mark Time 8cts at 8 to 5
+Forwards March 8 cts at 8 to 5
+Halt
+(Run On Tempo is 200 BPM)
+Run On (Command is “UP”)
+Run on Mark Time for 8cts
+Run On Forward March for 32cts
+Sing the Drum Cadence for the “Kick Down Up”', 1),
+    (NULL, 6, 'Evaluator Setup', 'evaluator', 'setup', '', 2),
+    (NULL, 6, 'Evaluator Script', 'evaluator', 'script', '', 3);
