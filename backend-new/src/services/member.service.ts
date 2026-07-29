6d564a144a26b573986e8dd02e9c17648aa86a68
sfishe10 <sfishe10@calpoly.edu>
2026-07-29 11:30:55 -0700

    tweaked column names for parsing 2026 supplemental form

--- backend-new/src/services/member.service.ts
+++ backend-new/src/services/member.service.ts
@@ -161,7 +161,7 @@ export class MemberService {
         );
 
         const invalidSections: Set<string> = new Set(rows
-            .map(row => row.Section?.trim().toLowerCase())
+            .map(row => row['Section']?.trim().toLowerCase())
             .filter((s): s is string => !!s)
             .filter(sectionName => !(sectionMap.has(sectionName))));
 
@@ -175,19 +175,13 @@ export class MemberService {
         const members: Member[] = [];
 
         for (const row of rows) {
-            // TODO: extract these column names out as constants
-            const officialLastName = row['Official Last']?.trim() ?? '';
-            const officialFirstName = row['Official First']?.trim() ?? '';
-            const preferredLastName = row['Preferred Last']?.trim() ?? '';
-            const preferredFirstName = row['Preferred First']?.trim() ?? '';
+            const lastName = row['Last Name']?.trim() ?? '';
+            const firstName = row['First Name']?.trim() ?? '';
             // for extended ed students without a CP email, use their preferred email for now -
             // they will not be needing to log in, so it won't cause problems
-            const email = row.Email!.trim() !== 'anonymous' ? row.Email!.trim() : row['Preferred Email']!.trim();
+            const email = row['CP Email']!.trim() !== '' ? row['CP Email']!.trim() : row['Preferred Email Address']!.trim();
 
-            const lastName = preferredLastName === '0' ? officialLastName : preferredLastName;
-            const firstName = preferredFirstName === '0' ? officialFirstName : preferredFirstName;
-
-            const sectionName = row.Section?.trim().toLowerCase() ?? '';
+            const sectionName = row['Section']!.trim().toLowerCase() ?? '';
             // ! = non-null assertion, since we already checked for invalid section names
             const section: Section = sectionMap.get(sectionName)!;
 
